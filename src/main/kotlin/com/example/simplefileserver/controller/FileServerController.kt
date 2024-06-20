f6d8b98cd5d91d82b2a2358629594bc659b22e29
Lanlinju <lanlinju@outlook.com>
2024-06-20 22:07:34 +0800

    实现分片传输下载文件的功能

--- src/main/kotlin/com/example/simplefileserver/controller/FileServerController.kt
+++ src/main/kotlin/com/example/simplefileserver/controller/FileServerController.kt
@@ -16,23 +16,33 @@ class FileServerController {
 
     @RequestMapping("/**")
     fun handleFileRequest(request: HttpServletRequest, response: HttpServletResponse) {
+//
+//        request.characterEncoding = "UTF-8"
+//        response.characterEncoding = "UTF-8"
+
         val path: String = rootPath + request.requestURI.replace("/file", "")
         val file = File(URLDecoder.decode(path, StandardCharsets.UTF_8))
         println(path)
 
-        if (file.exists()) {
-            if (file.isDirectory) {
-                sendDirectoryList(file, response)
-            } else {
-                downloadFile(file, response)
+        when {
+            file.exists() -> {
+                if (file.isDirectory) {
+                    sendDirectoryList(file, response)
+                } else {
+                    serveFile(file, request, response)
+                }
             }
-        } else {
-            response.writer.println("HTTP/1.0 404 Not Found")
-        }
 
+            else -> sendNotFound(response)
+        }
     }
 
-    private fun downloadFile(file: File, response: HttpServletResponse) {
+    private fun sendNotFound(response: HttpServletResponse) {
+        response.status = HttpServletResponse.SC_NOT_FOUND
+        response.writer.println("HTTP/1.0 404 Not Found")
+    }
+
+    private fun serveFile(file: File, request: HttpServletRequest, response: HttpServletResponse) {
         val mimeType = getMimeType(file)
         response.contentType = mimeType
         response.setContentLengthLong(file.length())
@@ -40,19 +50,76 @@ class FileServerController {
         val contentDisposition = if (isInlineDisplay(mimeType)) "inline" else "attachment"
         response.setHeader("Content-Disposition", "$contentDisposition; filename=\"${file.name}\"")
 
+        val rangeRequest = request.getHeader("Range") != null
+
+        if (rangeRequest) {
+            println("------------------serveFileInChunks-----------------")
+            serveFileInChunks(file, request, response)
+        } else {
+            println("------------------serveFileNormally-----------------")
+            serveFileNormally(file, response)
+        }
+    }
+
+    /**
+     * 普通下载方式
+     */
+    private fun serveFileNormally(file: File, response: HttpServletResponse) {
         val output = response.outputStream
         val input = file.inputStream()
-        // 将文件内容写入响应体
         val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
         var bytesRead: Int
         while (input.read(buffer).also { bytesRead = it } != -1) {
             output.write(buffer, 0, bytesRead)
         }
+        input.close()
+    }
 
+    /**
+     * 分片下载方式
+     *
+     * --- Request Header ---
+     * GET /example-file.txt HTTP/1.1
+     * Host: www.example.com
+     * Range: bytes=0-499
+     *
+     * --- Response Header ---
+     * HTTP/1.1 206 Partial Content
+     * Content-Range: bytes 0-499/1234 // start-end/size, 0-/1234 equals 0-499/1234
+     * Content-Length: 500 // 注意此时content-length为：end - start + 1
+     * Content-Type: text/plain
+     */
+    private fun serveFileInChunks(file: File, request: HttpServletRequest, response: HttpServletResponse) {
+        val rangeHeader = request.getHeader("Range")
+        val ranges = rangeHeader.removePrefix("bytes=").split("-") // 0-144 to ["0", "144"]; 0- to ["0", ""]
+        val start = ranges[0].toLong()
+        val end = if (ranges[1].isNotEmpty()) ranges[1].toLong() else file.length() - 1
+
+        response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT)
+        response.setContentLengthLong(end - start + 1)
+        response.setHeader("Accept-Ranges", "bytes")
+        response.setHeader("Content-Range", "bytes $start-$end/${file.length()}")
+
+        val output = response.outputStream
+        val input = file.inputStream()
+        input.skip(start)
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        var bytesToRead = end - start + 1
+
+        while (bytesToRead > 0) {
+            val bytesRead = input.read(buffer)
+            if (bytesRead > bytesToRead) {
+                output.write(buffer, 0, bytesToRead.toInt())// eg. 0-100/144, bytesToRead = 101, bytesRead = 145
+                break
+            }
+            output.write(buffer, 0, bytesRead)
+            bytesToRead -= bytesRead
+        }
         input.close()
     }
 
     private fun getMimeType(file: File): String {
+//        val contentType = URLConnection.guessContentTypeFromName(file.name)
         val fileName = file.name.lowercase()
         return when {
             fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") -> "image/jpeg"
--- src/test/kotlin/com/example/simplefileserver/SimpleFileserverApplicationTests.kt
+++ src/test/kotlin/com/example/simplefileserver/SimpleFileserverApplicationTests.kt
@@ -1,13 +1,49 @@
 package com.example.simplefileserver
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SimpleFileserverApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
+    @Test
+    fun test() {
+        val input = "bytes=0-499"
+        val expected = "0"
+        val expected2 = "499"
+
+        val ranges = input.removePrefix("bytes=").split("-")
+        val actual = ranges[0]
+        val actual2 = ranges[1]
+
+        assertEquals(expected, actual)
+        assertEquals(expected2, actual2)
+
+        val input2 = "bytes=0-"
+        val expected3 = 1234L
+        val ranges2 = input2.removePrefix("bytes=").split("-")
+        val actual3 = if (ranges2.size > 1 && ranges2[1].isNotEmpty()) ranges2[1].toLong() else 1234
+        assertEquals(2, ranges2.size) // 0- to ["0", ""]
+        assertEquals(true, ranges2[1].isEmpty())
+
+        assertEquals(expected3, actual3)
+
+    }
+
+    @Test
+    fun testIntRange() {
+        val input = 1..8
+        val expected = 1
+        val expected2 = 8
+
+        assertEquals(expected, input.first)
+        assertEquals(expected, input.start)
+        assertEquals(expected2, input.last)
+        assertEquals(expected2, input.endInclusive)
+    }
 }
